45ffd8d59c05913e004cb7d4521c96781f760d26
Lina Viveros <linaviverosm@gmail.com>
2024-09-16 22:03:20 -0500

    instalacion de prisma en usuarios

--- Usuario/controllers/usuario.controller.js
+++ Usuario/controllers/usuario.controller.js
@@ -8,21 +8,44 @@
  */
 
 const {response, request} =require('express');
+const {PrismaClient} = require ('@prisma/client');
+
+const prisma = new PrismaClient();
 
 
 const RegistrarUsuario = async (req=request, res=response)=>{
+    const {email, password } =req.body;
+
+    const result = await prisma.user.create({
+        data: {
+            email,
+            password
+        }
+    })    .catch(err=>{
+        return err.message;
+    }).finally((async()=>{
+        await prisma.$disconnect();
+    }));
+
     res.json({
-        "nombre": "admin",
-        "contraseña":"admin",
+        result
 
 
     });
 } ;
 
 const IniciarSesion = async (req=request, res=response)=>{
+
+    const user = await prisma.user.findMany()
+    .catch(err=>{
+        return err.message;
+    }).finally((async()=>{
+        await prisma.$disconnect();
+    }));
+
+    
     res.json({
-        "nombre": "admin",
-        "contraseña":"admin",
+        user
 
 
     });
